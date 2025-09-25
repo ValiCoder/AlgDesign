12f036c2d42f3ea9c496b9258ce1ed1690c31674
ValiCoder <Misterivd7@gmail.com>
2025-09-26 01:03:38 +0500

    Task1 QuickSortAlgorithm Push

---
+++ src/Quick_Sort.java
@@ -0,0 +1,46 @@
+public class Quick_Sort {
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 7, 8, 9, 1, 5};
+
+        System.out.println("Unsorted array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        quickSort(numbers, 0, numbers.length - 1);
+
+        System.out.println("\nSorted array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
